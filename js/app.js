76eab3432a959926000c3e09e6d52ff99b38c7de
Nic <deux02030@gmail.com>
2017-08-31 15:38:19 -0400

    update with buttons and function to point to current selected cat

--- js/app.js
+++ js/app.js
@@ -1,14 +1,44 @@
-var Cat = function() {
+var initial_cats = [
+  {
+      clickCount : 0,
+      name : 'Henry',
+      imgSrc : 'img/cat1.jpg',
+      nicknames: "KC's Bae"
+  },
+  {
+      clickCount : 0,
+      name : 'Bajheera',
+      imgSrc : 'img/cat2.jpg',
+      nicknames: 'Swoljer'
+  },
+  {
+      clickCount : 0,
+      name : 'Kaan',
+      imgSrc : 'img/cat3.jpg',
+      nicknames: 'Tiger'
+  },
+  {
+      clickCount : 0,
+      name : 'Claw',
+      imgSrc : 'img/cat4.jpg',
+      nicknames: 'Scratch'
+  },
+  {
+      clickCount : 0,
+      name : 'Mittens',
+      imgSrc : 'img/cat5.jpg',
+      nicknames: 'Leon'
+  }
+]
+
+
+var Cat = function(data) {
   var self = this;
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Henry');
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
   self.level = ko.observable('Baby G');
-  this.imgSrc = ko.observable('img/cat1.jpg');
-  self.nicknames = ko.observableArray([
-    {name: "KC's Bae"},
-    {name: "Professor"},
-    {name: "H Man"}
-  ]);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.nickname = ko.observable(data.nicknames);
 
 
   self.level = ko.computed(function(){
@@ -34,12 +64,27 @@ var Cat = function() {
 
 }
 
-var ViewModel = function() {
 
-  this.currentCat = ko.observable( new Cat() );
+
+var ViewModel = function() {
+  var self = this;
+
+  this.cat_list = ko.observableArray([]);
+
+  initial_cats.forEach(function(cat_item) {
+    self.cat_list.push( new Cat(cat_item) );
+  });
+
+
+  this.currentCat = ko.observable(this.cat_list()[0] );
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
   };
+
+  this.set_cat = function(selected) {
+    self.currentCat(selected);
+  };
+
 };
 ko.applyBindings(new ViewModel())
