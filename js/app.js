48901737aa645b0327f11c6d07e6e6fd414c6725
Nic <deux02030@gmail.com>
2017-08-31 13:09:58 -0400

    add data bind with to html / update js

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ var ViewModel = function() {
   this.currentCat = ko.observable( new Cat() );
 
   this.incrementCounter = function() {
-    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    this.clickCount(this.clickCount() + 1);
   };
 };
 ko.applyBindings(new ViewModel())
